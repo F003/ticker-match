2fd754876e0108b5f95152bec5485f622d879561
F003 <stafilip.bop@gmail.com>
2023-05-30 15:44:52 +0200

    working

--- tickermatch.py
+++ tickermatch.py
@@ -9,6 +9,24 @@ df_to_match['MatchingName'] = df_to_match['MatchingName'].astype(str)
 df_db['CompanyName'] = df_db['CompanyName'].astype(str)
 df_db['Ticker'] = df_db['Ticker'].astype(str)
 
+# create new dataframe with CompanyName and Ticker collums from df_db
+df_tickersdb = df_db[['CompanyName', 'Ticker']]
 
-df_tickersdb = df_db[['Ticker'] ['CompanyName']]
-print(df_tickersdb)
+df_merged = pd.merge(df_to_match, df_tickersdb, left_on='MatchingName', right_on='CompanyName', how='left')
+df_merged.rename(columns={'Ticker': 'Ticker'}, inplace=True)
+
+# drop CompanyName_y collumn from df_merged
+
+df_merged.drop(columns=['CompanyName_y'], inplace=True)
+
+#rename 'CompanyName_x' collumn to 'CompanyName'
+
+df_merged.rename(columns={'CompanyName_x': 'CompanyName'}, inplace=True)
+
+# Make SimilarityScore 4th collumn and Ticker 3rd collumn in df_merged
+df_placeholder = df_merged['SimilarityScore'].copy()
+df_merged.drop(columns=['SimilarityScore'], inplace=True)
+df_merged['SimilarityScore'] = df_placeholder
+
+# save df_merged to csv file
+df_merged.to_csv('outputs/final.csv', index=False)
