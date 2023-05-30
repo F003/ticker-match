48e994c20270eda1278566c71f89697cb399dc3c
F003 <stafilip.bop@gmail.com>
2023-05-30 15:10:17 +0200

    saving before quick fix

--- tickermatch.py
+++ tickermatch.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-df_to_match = pd.read_csv('output/names_matched.csv', encoding='utf-8')
-df_db = pd.read_csv(data/db.csv, encoding='utf-8')
+df_to_match = pd.read_csv('outputs/names_matched.csv', encoding='utf-8')
+df_db = pd.read_csv('data/db.csv', encoding='utf-8')
 
 df_to_match['MatchingName'] = df_to_match['MatchingName'].astype(str)
-df_db['Ticeker', 'CompanyName'] = df_db['Ticker', 'CompanyName'].astype(str)
+
+# using pandas as pd and astype(str) method change 'CompanyName' and 'Ticker' collums from df_db to strings
+df_db['CompanyName'] = df_db['CompanyName'].astype(str)
+df_db['Ticker'] = df_db['Ticker'].astype(str)
+
 
 df_tickersdb = df_db[['Ticker'] ['CompanyName']]
 print(df_tickersdb)
